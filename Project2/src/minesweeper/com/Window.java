76e7d1fa4c8a8be3cb8d0eae4907d4431979da0c
fruitella44 <zelenij24@yandex.ru>
2022-02-24 21:35:18 +0300

    mineSweeper

---
+++ Project2/src/minesweeper/com/Window.java
@@ -0,0 +1,22 @@
+package minesweeper.com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window {
+    private static JFrame frame;
+    private static String title;
+
+    public Window(int width, int height, int gridsize, String title, Game game) {
+        Window.title = title;
+        frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+}
