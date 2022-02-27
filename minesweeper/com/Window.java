4bb04cc22a1b36881ca1bbe7a77d5ce1acea1b93
fruitella44 <zelenij24@yandex.ru>
2022-02-27 14:59:12 +0300

    Update Window.java

--- minesweeper/com/Window.java
+++ minesweeper/com/Window.java
@@ -17,6 +17,7 @@ public class Window {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.DARK_GRAY);
 
     }
 }
